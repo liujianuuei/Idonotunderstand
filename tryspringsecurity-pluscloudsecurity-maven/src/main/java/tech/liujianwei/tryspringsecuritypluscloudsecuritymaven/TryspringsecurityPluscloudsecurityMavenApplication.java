c6a708df2b0bec6fb98462cca7936202d09ad05f
jianwliu <jianwei.liu@oracle.com>
2019-05-05 22:00:08 +0800

    try spring security

---
+++ tryspringsecurity-pluscloudsecurity-maven/src/main/java/tech/liujianwei/tryspringsecuritypluscloudsecuritymaven/TryspringsecurityPluscloudsecurityMavenApplication.java
@@ -0,0 +1,13 @@
+package tech.liujianwei.tryspringsecuritypluscloudsecuritymaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryspringsecurityPluscloudsecurityMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryspringsecurityPluscloudsecurityMavenApplication.class, args);
+	}
+
+}
