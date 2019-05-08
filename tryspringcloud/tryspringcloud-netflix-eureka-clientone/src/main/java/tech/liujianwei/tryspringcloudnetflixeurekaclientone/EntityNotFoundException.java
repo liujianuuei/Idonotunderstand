15afda62542d6c347db96766c2a3c1a0aa784b67
jianwliu <jianwei.liu@oracle.com>
2019-05-08 18:41:01 +0800

    add error handling

---
+++ tryspringcloud/tryspringcloud-netflix-eureka-clientone/src/main/java/tech/liujianwei/tryspringcloudnetflixeurekaclientone/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package tech.liujianwei.tryspringcloudnetflixeurekaclientone;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String msg) {
+        super(msg);
+    }
+}
