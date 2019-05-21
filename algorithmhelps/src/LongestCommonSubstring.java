5bc7f9b6cf5f1bd888a63b22052cc4819477c062
jianwliu <jianwei.liu@oracle.com>
2019-05-21 20:49:10 +0800

    kth largest element

--- algorithmhelps/src/LongestCommonSubstring.java
+++ algorithmhelps/src/LongestCommonSubstring.java
@@ -1,7 +1,3 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-
 public class LongestCommonSubstring {
 
     private String getCommonStrLength(String str1, String str2) {
---
+++ algorithmhelps/src/sort/KthLargestElementInAnArray.java
@@ -0,0 +1,74 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class KthLargestElementInAnArray {
+
+    public static int findKthLargest1(int[] nums, int k) {
+        final int N = nums.length;
+        Arrays.sort(nums);
+        return nums[N - k];
+    }
+
+    public static int findKthLargest2(int[] nums, int k) {
+        final PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int val : nums) {
+            pq.offer(val);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+
+    // Ref. https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained
+    public static int findKthLargest(int[] nums, int k) {
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if (j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    private static int partition(int[] a, int lo, int hi) {
+
+        int i = lo;
+        int j = hi + 1;
+        while (true) {
+            while (i < hi && less(a[++i], a[lo])) ;
+            while (j > lo && less(a[lo], a[--j])) ;
+            if (i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private static boolean less(int v, int w) {
+        return v < w;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+        int n = findKthLargest(arr, 4);
+        System.out.println(n);
+    }
+}
