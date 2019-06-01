a1c87150890931b0969a63ba3f37bc7e2b3ba4eb
jianliuwei <liujianwei.me@gmail.com>
2019-06-01 19:50:34 +0800

    Place Application Context, i.e., applicationContext.xml and Web Application Context, i.e, dispatcher.servlet.xml in src/main/webapp/WEB-INF

--- tryssm/BookHub/src/test/java/tech/liujianwei/service/impl/BookServiceImplTest.java
+++ tryssm/BookHub/src/test/java/tech/liujianwei/service/impl/BookServiceImplTest.java
@@ -1,13 +1,9 @@
 package tech.liujianwei.service.impl;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 import tech.liujianwei.model.Book;
 import tech.liujianwei.service.BookService;
 
@@ -15,7 +11,7 @@ public class BookServiceImplTest {
 
     @Test
     public void testGetBook() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/applicationContext.xml");
         BookService bookService = (BookService) applicationContext.getBean("bookServiceImpl");
         Book book = bookService.getBook(1);
         System.out.println(book);
