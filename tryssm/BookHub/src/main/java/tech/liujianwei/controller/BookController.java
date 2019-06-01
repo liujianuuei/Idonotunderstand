271d5ff3cbac4dd3ecd8b33072c2e823059762d9
jianliuwei <liujianwei.me@gmail.com>
2019-06-01 23:01:42 +0800

    Use bean in controller

--- tryssm/BookHub/src/main/java/tech/liujianwei/controller/BookController.java
+++ tryssm/BookHub/src/main/java/tech/liujianwei/controller/BookController.java
@@ -1,21 +1,25 @@
 package tech.liujianwei.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import tech.liujianwei.model.Book;
-import tech.liujianwei.service.impl.BookServiceImpl;
+import tech.liujianwei.service.BookService;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
 
+    @Autowired
+    BookService bookService;
+
     @GetMapping("/{id}")
     public ModelAndView get(@PathVariable String id) {
         System.out.println("BookController:/book/" + id);
-        Book book = new BookServiceImpl().getBook(Integer.valueOf(id));
+        Book book = bookService.getBook(Integer.valueOf(id));
         // Can also use Model+"book" instead of ModelAndView
         ModelAndView bookView = new ModelAndView("book");
         bookView.addObject("book", book);
