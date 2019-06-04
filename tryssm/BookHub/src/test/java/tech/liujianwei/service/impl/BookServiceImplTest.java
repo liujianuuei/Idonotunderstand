7a4d1d23f33603340b933f1485a606e30cc0b5ad
jianliuwei <liujianwei.me@gmail.com>
2019-06-04 15:30:47 +0800

    Add Log4j support

--- tryssm/BookHub/src/test/java/tech/liujianwei/service/impl/BookServiceImplTest.java
+++ tryssm/BookHub/src/test/java/tech/liujianwei/service/impl/BookServiceImplTest.java
@@ -1,18 +1,28 @@
 package tech.liujianwei.service.impl;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import tech.liujianwei.model.Book;
 import tech.liujianwei.service.BookService;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/applicationContext.xml"})
 public class BookServiceImplTest {
+
+    @Resource(name = "BookService")
+    BookService bookService;
 
     @Test
     public void testGetBook() {
-        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/applicationContext.xml");
-        BookService bookService = (BookService) applicationContext.getBean("BookService");
+        //ApplicationContext applicationContext = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/applicationContext.xml");
+        //BookService bookService = (BookService) applicationContext.getBean("BookService");
         Book book = bookService.getBook(1);
         System.out.println(book);
     }
